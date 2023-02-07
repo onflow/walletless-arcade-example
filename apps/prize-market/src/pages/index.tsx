60fe66f3aeaf0cbe4713569cd66c7908ff3cc9e7
gregsantos <greg.santos@dapperlabs.com>
2023-02-06 16:36:53 -0800

    chore: update icon url

--- apps/prize-market/src/pages/index.tsx
+++ apps/prize-market/src/pages/index.tsx
@@ -13,12 +13,6 @@ const Home: NextPage = () => {
     disconnect,
   }
 
-  console.log(
-    process.env.NEXT_PUBLIC_FLOW_NETWORK,
-    process.env.NEXT_PUBLIC_APP_NAME,
-    process.env.VERCEL_URL
-  )
-
   return (
     <>
       <Head>
