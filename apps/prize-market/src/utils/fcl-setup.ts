74ea354d52ba6b4d08dbb681ac81ef8391ea54b7
Greg Santos <greg.santos@dapperlabs.com>
2023-02-06 16:49:36 -0800

    Add Tickets Exchange (#20)
    * chore: update flow.json
    
    * chore: update icon url

--- apps/prize-market/src/utils/fcl-setup.ts
+++ apps/prize-market/src/utils/fcl-setup.ts
@@ -1,9 +1,14 @@
 import * as fcl from '@onflow/fcl'
 import { FLOW } from '../constants'
-import { getUrl } from './get-url'
 import flowJSON from '../../../../flow.json'
 
-const iconUrl = getUrl() + '/public/flow-icon.png'
+const iconUrl =
+  process.env.NEXT_PUBLIC_FLOW_NETWORK === 'emulator'
+    ? 'http://localhost:' +
+      process.env.NEXT_PUBLIC_PORT +
+      '/static/flow-icon-bw-green.svg'
+    : 'https://walletless-arcade-marketplace.vercel.app' +
+      '/static/flow-icon-bw-green.svg'
 const appTitle = process.env.NEXT_PUBLIC_APP_NAME || 'Flow Arcade Market'
 const flowNetwork = process.env.NEXT_PUBLIC_FLOW_NETWORK as fcl.Environment
 
