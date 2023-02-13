824f472b236960998a78eb3f3bb24ab450267a7c
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2023-02-13 11:09:51 -0800

    Move fcl context to ui

--- apps/rps-game/src/components/game-view.tsx
+++ apps/rps-game/src/components/game-view.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react'
-import { FlashButton, Row, Modal } from 'ui'
-import { useRpsGameContext, GameStatus, useTicketContext, useFclContext } from '../contexts'
+import { FlashButton, Row, Modal, useFclContext } from 'ui'
+import { useRpsGameContext, GameStatus, useTicketContext } from '../contexts'
 
 type PlayerMove = 'rock' | 'paper' | 'scissors' | undefined
 
@@ -151,11 +151,7 @@ const GameView = () => {
           Now that you've connected your wallet, head to the marketplace to spend your tickets on an NFT prize!
         `}
         buttonText={"Go to Marketplace"}
-<<<<<<< HEAD
         buttonFunc={() => window.location.replace(process.env.NEXT_PUBLIC_MARKETPLACE_URL || "")}
-=======
-        buttonFunc={() => window.location.replace(process.env.NEXT_PUBLIC_MARKETPLACE_URL)}
->>>>>>> 0110c4c (Adds wallet connected redirect modal)
       />
       <Modal 
         isOpen={gameStatus === GameStatus.READY || gameStatus === GameStatus.ENDED}
