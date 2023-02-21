a4bd415e9641dcdec02d1d0544c8aca0e1cd292e
gregsantos <greg.santos@dapperlabs.com>
2023-02-21 11:31:50 -0800

    feat: update urls used for env

--- apps/prize-market/src/utils/authz-functions.ts
+++ apps/prize-market/src/utils/authz-functions.ts
@@ -1,29 +1,6 @@
 import type { AuthorizationObject } from '@onflow/fcl'
 import { getUrl } from './get-url'
 
-// export function adminAuthorizationFunction(account: any) {
-//   const adminPrivateKey = env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY_ID;
-//   const adminKeyIndex = env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY_ID;
-//   const adminAddress = env.NEXT_PUBLIC_ADMIN_ADDRESS;
-
-//   // authorization function need to return an account
-//   return {
-//     ...account, // bunch of defaults in here, we want to overload some of them though
-//     tempId: `${adminAddress}-${adminKeyIndex}`, // tempIds are more of an advanced topic, for 99% of the times where you know the address and keyId you will want it to be a unique string per that address and keyId
-//     addr: adminAddress, // the address of the signatory
-//     keyId: Number(adminKeyIndex), // this is the keyId for the accounts registered key that will be used to sign, make extra sure this is a number and not a string
-//     signingFunction: async (signable: any) => {
-//       // Singing functions are passed a signable and need to return a composite signature
-//       // signable.message is a hex string of what needs to be signed.
-//       return {
-//         addr: adminAddress, // needs to be the same as the account.addr
-//         keyId: Number(adminKeyIndex), // needs to be the same as account.keyId, once again make sure its a number and not a string
-//         signature: await sign(signable.message, adminPrivateKey), // this needs to be a hex string of the signature, where signable.message is the hex value that needs to be signed
-//       };
-//     },
-//   };
-// }
-
 export function adminAuthorizationFunction(account: any): AuthorizationObject {
   const address = process.env.NEXT_PUBLIC_ADMIN_ADDRESS
   const keyIndex = process.env.NEXT_PUBLIC_ADMIN_KEY_INDEX
--- apps/prize-market/src/utils/fcl-setup.ts
+++ apps/prize-market/src/utils/fcl-setup.ts
@@ -1,16 +1,14 @@
 import * as fcl from '@onflow/fcl'
 import { FLOW } from '../constants'
 import flowJSON from '../../../../flow.json'
+import { getUrl } from './get-url'
 
-const iconUrl =
-  process.env.NEXT_PUBLIC_FLOW_NETWORK === 'local'
-    ? 'http://localhost:' +
-      process.env.NEXT_PUBLIC_PORT +
-      '/static/flow-icon-bw-green.svg'
-    : 'https://walletless-arcade-marketplace.vercel.app' +
-      '/static/flow-icon-bw-green.svg'
-const appTitle = process.env.NEXT_PUBLIC_APP_NAME || 'Flow Arcade Market'
 const flowNetwork = process.env.NEXT_PUBLIC_FLOW_NETWORK as fcl.Environment
+const iconUrl =
+  flowNetwork === 'local'
+    ? `${getUrl()}/static/flow-icon-bw-green.svg`
+    : `${process.env.NEXT_PUBLIC_VERCEL_URL}/static/flow-icon-bw-green.svg`
+const appTitle = process.env.NEXT_PUBLIC_APP_NAME || 'Flow Arcade Market'
 
 export const loadFCLConfig = () => {
   fcl
