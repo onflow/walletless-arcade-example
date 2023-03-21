d74f8550510dec6d48fcc0485387de2b0ca34562
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2023-03-21 14:47:42 -0700

    Resolve on transaction execution

--- packages/shared/src/contexts/FclContext.tsx
+++ packages/shared/src/contexts/FclContext.tsx
@@ -137,7 +137,7 @@ export default function FclContextProvider({
                 )
               }
             }
-            if (res.status >= 4) {
+            if (res.status >= 3) {
               setTransactionEvents(res.events || null)
               setTransactionInProgress(false)
               setFullScreenLoading(false)
